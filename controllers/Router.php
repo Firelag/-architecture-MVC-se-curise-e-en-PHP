6c9df08c2d7e8f1feee0caf14f6998482cab8443
Firelag <jordan.vincent085@gmail.com>
2023-04-04 15:06:42 +0200

    commit 2

--- controllers/Router.php
+++ controllers/Router.php
@@ -7,11 +7,41 @@ class Router {
 
     public function routeReq(){
         try{
+            //Chargement automatique des classes
             spl_autoload_register(function($class){
-                require_once('models/'.class.'.php');
-            })
+                require_once('models/'.$class.'.php');
+            });
+
+            $url = '';
+
+            //Le controleur est inclu selon l'action de l'utilisateur
+
+            if(isset($_GET['url'])){
+                $url =  explode('/', filter_var($_GET['url'],FILTER_SANITIZE_URL));
+
+                $controller = ucfirst(strtolower($url[0]));
+                $controllerClass = "Controller".$controller;
+                $controllerFile = "controllers/".$controllerClass.".php";
+
+                if(file_exists($controllerFile))
+                {
+                    require_once ($controllerFile);
+                    $this->_ctrl -> new $controllerClass($url);
+                }
+                else
+                    throw new Exception('Page introuvable');
+                    $this->_ctrl -> new ControllerAcceuil($url);
+            }
+            else
+            {
+                require_once('controllers/ControllerAcceuil.php');
+            }
         }
+
+        //Gestion des erreurs
         catch(Exception $e){
+            $errorMsg = $e->getMessage();
+            require_once('view/viewAcceuil.php');
 
         }
     }
